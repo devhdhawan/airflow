56d9ee03d6ca4fea3211a09a06bc7582774e782f
devdhawan <himanshudimp05@gmail.com>
2025-04-13 11:04:41 +0530

    Astro Deployment:3

--- .github/deploy_script.sh
+++ .github/deploy_script.sh
@@ -4,6 +4,7 @@ curl -sSL install.astronomer.io | sudo bash -s
 # Determine if only DAG files have changes
 files=$(git diff --name-only $(git rev-parse HEAD~1) -- .)
 dags_only=1
+echo "DEPLOYMENT ID $DEPLOYMENT_ID"
 for file in $files; do
   if [[ $file != "$DAG_FOLDER"* ]]; then
     echo "$file is not a dag, triggering a full image build"
@@ -14,10 +15,10 @@ done
 # If only DAGs changed deploy only the DAGs in your 'dags' folder to your Deployment
 if [ $dags_only == 1 ]
 then
-    astro deploy --dags
+    astro deploy --dags $DEPLOYMENT_ID
 fi
 # If any other files changed build your Astro project into a Docker image, push the image to your Deployment, and then push and DAG changes
 if [ $dags_only == 0 ]
 then
-    astro deploy
+    astro deploy $DEPLOYMENT_ID
 fi
